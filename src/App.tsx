e19bae64d44013764a2ddc49498b425f2e544535
Anderson Silva <andersonsilv.mota@gmail.com>
2025-09-07 21:33:55 -0300

    chore: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { jsPDF } from "jspdf";
+import { Document, Packer, Paragraph, TextRun } from "docx";
+import { saveAs } from "file-saver";
 
-function App() {
-  const [count, setCount] = useState(0)
+type QuestionType = "ME" | "MR" | "AB";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Question {
+  id: number;
+  nivel: string;
+  modulo: string;
+  assunto: string;
+  tipo: QuestionType;
+  enunciado: string;
+  alternativas?: string[];
+  resposta?: string;
 }
 
-export default App
+export default function App() {
+
+
+  return (
+    <div className="min-h-screen ">
+      <div className=" p-8 bg-blue-200 mb-6">
+        <h1 className="text-3xl font-extrabold text-white text-center drop-shadow-lg">
+          GEM - Banco de Questões
+        </h1>
+        </div>
+     
+    </div>
+  );
+}
